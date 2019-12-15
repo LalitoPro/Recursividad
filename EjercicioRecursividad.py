306b05016823d9fed073146ab5d27e01bd7a430d
LalitoPro <47514452+LalitoPro@users.noreply.github.com>
2019-12-15 16:48:57 -0600

    feat: se pasa a orientado a objetos

--- EjercicioRecursividad.py
+++ EjercicioRecursividad.py
@@ -1,5 +1,45 @@
 import random, sys
+class dos_tres_cinco:
+    __multiplosDos = 0
+    __multiplosTres = 0
+    __multiplosCinco =  0
 
+# metodo donde se resive el nuemero a evaluar
+    def condicion(self,numero):
+        # pasamos a cadema de testo el numero
+        a =str(numero)
+        # se agreaga a una lista
+        A = list(a)
+        #iniciamos el recorrido del la cadena de texto desde la ultia posicion.
+        for i in range(-1, (len(A) + 1) * -1, -1):
+            # se avalua si son porsentuales de 2
+            if abs(i) % 2 == 0:
+                # si cumplen con la condicion se van acomulando en un contador
+                self.__multiplosDos += int(A[i])
+            # se avalua si son porsentuales de 3
+            if abs(i) % 3 == 0:
+                self.__multiplosTres += int(A[i])
+            # se avalua si son porsentuales de 5
+            if abs(i) % 5 == 0:
+                self.__multiplosCinco += int(A[i])
+        if (self.__multiplosDos == self.__multiplosTres + self.__multiplosCinco):
+            print(a, " es un numeero dos tres cinco")
+        else:
+            print(a, "no es un numero dos tres cinco")
+
+def main():
+
+    prueba = dos_tres_cinco()
+    prueba.condicion(56321)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+"""
+codico estructurado
 a= str(56321)
 A = list(a)
 
@@ -18,3 +58,4 @@ if(multiplosDos == multiplosTres + multiplosCinco):
     print(a, " es un numeero dos tres cinco")
 else:
     print(a, "no es un numero dos tres cinco")
+"""
