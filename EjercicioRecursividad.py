be7eb0ce4ce4c8a6c2d8ec32e30770f5a49afee6
LalitoPro <47514452+LalitoPro@users.noreply.github.com>
2019-12-14 19:44:42 -0600

    feat: se crea el programa en su forma estructurada y interativo

--- EjercicioRecursividad.py
+++ EjercicioRecursividad.py
@@ -1,1 +1,20 @@
-class recursividad 
+import random, sys
+
+a= str(56321)
+A = list(a)
+
+multiplosDos = 0
+multiplosTres = 0
+multiplosCinco = 0
+for i in range(-1, (len(A) + 1)*-1, -1):
+    if abs(i) % 2 == 0:
+        multiplosDos += int(A[i])
+    if abs(i) % 3 == 0:
+        multiplosTres += int(A[i])
+    if abs(i) % 5 == 0:
+        multiplosCinco += int(A[i])
+
+if(multiplosDos == multiplosTres + multiplosCinco):
+    print(a, " es un numeero dos tres cinco")
+else:
+    print(a, "no es un numero dos tres cinco")
